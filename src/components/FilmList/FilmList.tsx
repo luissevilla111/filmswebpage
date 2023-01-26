5ec1ddd5faa02bdcd83a9d46011e9d24b262a7cd
luissevilla <luissevilla111@gmail.com>
2023-01-26 14:37:03 -0600

    adding the filmpage with flex

---
+++ src/components/FilmList/FilmList.tsx
@@ -0,0 +1,23 @@
+import classes from "./FilmList.module.css";
+
+const FilmList = () => {
+  return (
+    <div className={classes["list-films"]}>
+      <p>HHHH</p>
+      <p>IIII</p>
+      <p>JJJJJ</p>
+      <p>JJJJJ</p>
+      <p>JJJJJ</p>
+      <p>JJJJJ</p>
+      <p>JJJJJ</p>
+      <p>JJJJJ</p>
+      <p>JJJJJ</p>
+      <p>JJJJJ</p>
+      <p>JJJJJ</p>
+      <p>JJJJJ</p>
+      <p>JJJJJ</p>
+    </div>
+  );
+};
+
+export default FilmList;
---
+++ src/pages/FilmsPage.tsx
@@ -0,0 +1,7 @@
+import FilmList from "../components/FilmList/FilmList";
+
+const FilmsPage = () => {
+  return <FilmList />;
+};
+
+export default FilmsPage;
