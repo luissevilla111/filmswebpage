a7c38e4e2b92acd80becfa13a9b1c75ffcf18ba2
luissevilla <luissevilla111@gmail.com>
2023-01-27 16:32:20 -0600

    adding button for next page

--- src/components/FilmList/FilmList.tsx
+++ src/components/FilmList/FilmList.tsx
@@ -1,22 +1,37 @@
 import axios from "axios";
 import { useCallback, useEffect, useState } from "react";
-import { Film, GetFilmsResponse } from "../../models/Film";
+import { Film, GetFilmsResponse, ILastKey } from "../../models/Film";
+import Button from "../UI/Button/Button";
 import Card from "../UI/card/Card";
 import classes from "./FilmList.module.css";
 
+interface IPagination {
+  firstPage: null;
+  nextPage: ILastKey;
+  previousPage: ILastKey;
+}
+
 const FilmList = () => {
   const [films, setFilms] = useState<Film[]>([]);
+  const [pagination, setPagination] = useState<ILastKey | null>(null);
 
-  const fetchFilmsHandler = useCallback(async () => {
+  const fetchFilmsHandler = useCallback(async (params: ILastKey | null) => {
     try {
-      const filmsFetchedObj = await axios.get<GetFilmsResponse>(
-        `${process.env.REACT_APP_API_FILMS}/films`
-      );
+      let uri = `${process.env.REACT_APP_API_FILMS}/films`;
+
+      if (params) {
+        uri += `?Saga=${params.Saga}&Name=${params.Name}`;
+      }
+
+      const filmsFetchedObj = await axios.get<GetFilmsResponse>(uri);
 
       console.log(filmsFetchedObj.data.films);
       const allFilms = filmsFetchedObj.data.films;
-      const nextLink = filmsFetchedObj.data.lastKey;
+      const nextLink = filmsFetchedObj.data.lastKey
+        ? filmsFetchedObj.data.lastKey
+        : null;
 
+      setPagination(nextLink);
       console.log(nextLink);
       setFilms(allFilms);
     } catch (err) {
@@ -25,37 +40,40 @@ const FilmList = () => {
   }, []);
 
   useEffect(() => {
-    fetchFilmsHandler();
+    fetchFilmsHandler(null);
   }, [fetchFilmsHandler]);
 
   return (
-    <div className={classes["list-films"]}>
-      {films.map((film) => {
-        const {
-          Name,
-          Description,
-          Added_Time_Utc,
-          Duration_Minutes,
-          Genders,
-          Image_Url,
-          Saga,
-          Stars,
-        } = film;
-        return (
-          <Card
-          key={`${Saga}_${Name}`}
-            Name={Name}
-            Description={Description}
-            Added_Time_Utc= {Added_Time_Utc}
-            Duration_Minutes={Duration_Minutes}
-            Genders={Genders}
-            Image_Url={Image_Url}
-            Saga={Saga}
-            Stars={Stars}
-          />
-        );
-      })}
-    </div>
+    <>
+      <div className={classes["list-films"]}>
+        {films.map((film) => {
+          const {
+            Name,
+            Description,
+            Added_Time_Utc,
+            Duration_Minutes,
+            Genders,
+            Image_Url,
+            Saga,
+            Stars,
+          } = film;
+          return (
+            <Card
+              key={`${Saga}_${Name}`}
+              Name={Name}
+              Description={Description}
+              Added_Time_Utc={Added_Time_Utc}
+              Duration_Minutes={Duration_Minutes}
+              Genders={Genders}
+              Image_Url={Image_Url}
+              Saga={Saga}
+              Stars={Stars}
+            />
+          );
+        })}
+      </div>
+      <Button text={!pagination ? "Back to Page 1" : "Next..."} fxClick={fetchFilmsHandler} params={pagination} />
+    </>
   );
 };
 
