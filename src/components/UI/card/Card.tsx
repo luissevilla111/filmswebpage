bead6b21a7f276fa1a10a700873572aa3fdd1b61
luissevilla <luissevilla111@gmail.com>
2023-01-27 14:35:01 -0600

    changing the image for a backgrount url

--- src/components/UI/card/Card.tsx
+++ src/components/UI/card/Card.tsx
@@ -3,25 +3,28 @@ import classes from "./Card.module.css";
 import Stat from "./Stat/Stat";
 import React from "react";
 
-
 const Card: React.FC<Film> = (props) => {
+  const myStyle = {
+    backgroundImage: `url(${props.Image_Url})`,
+    backgroundSize: "cover",
+  };
+
   return (
     <div className={`${classes["card-container"]} ${classes["grayscale"]}`}>
-      <div className={classes["card-img"]}>
-        <img
-          src={props.Image_Url}
-          alt="no imag"
-        ></img>
+      <div className={classes["card-img"]} style={myStyle}>
+        {/* <img src={props.Image_Url} alt="no imag"></img> */}
       </div>
-      <div className={classes["card-info"]}>
+      <div className={classes["card-info"]}  >
         <h2>{props.Name}</h2>
-        <p>
-          {props.Description}
-        </p>
+        <p>{props.Description}</p>
       </div>
       <div className={classes["card-stats"]}>
-        <Stat desc="Stars" value={+props.Stars} isStar = {true}></Stat>
-        <Stat desc="Minutes" value={+props.Duration_Minutes} isStar = {false}></Stat>
+        <Stat desc="Stars" value={+props.Stars} isStar={true}></Stat>
+        <Stat
+          desc="Minutes"
+          value={+props.Duration_Minutes}
+          isStar={false}
+        ></Stat>
       </div>
     </div>
   );
